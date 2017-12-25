9b6cce06ea16bc8c968c155d7a5e6350b7dd35e2
rial99 <ratulstephenite99@gmail.com>
2017-12-25 10:55:29 +0530

    favicon.ico added .. flask backend started

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	return "hello world!"
+
+@app.route("/admin/")
+def admin():
+	return "sorry admin page not yet createds"
+
+if __name__ == "__main__":
+	app.run(debug=True,port=80)
